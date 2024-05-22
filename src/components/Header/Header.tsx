4867e5decd7e24b9fa9a003dd2351d25c33609e9
Mela Maluenda <maluenda.mela@gmail.com>
2024-05-22 20:24:00 +0200

    Add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import { useTranslation } from "react-i18next";
+import Navigation from "../Navigation/Navigation";
+import HeaderStyled from "./HeaderStyled";
+
+const Header = (): React.ReactElement => {
+  const [t] = useTranslation("home");
+
+  return (
+    <HeaderStyled className="head">
+      <Navigation />
+      <h2 className="head_titular">{t("header.title")}</h2>
+      <div className="head__banner" />
+    </HeaderStyled>
+  );
+};
+
+export default Header;
