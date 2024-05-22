8786bed97db4fdc9167ea86b7a3745f7813ae94e
Mela Maluenda <maluenda.mela@gmail.com>
2024-05-22 20:26:14 +0200

    Add app component and main index

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,19 @@
-function App() {
+import HomePage from "../../pages/HomePage/HomePage";
+import Header from "../Header/Header";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+const App = (): React.ReactElement => {
   return (
     <>
-      <h1>Llar canina</h1>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </main>
     </>
   );
-}
+};
 
 export default App;
