f8d5109359c0b889f8fdb4cafa7def84cbd9411d
Mela Maluenda <maluenda.mela@gmail.com>
2024-05-23 23:46:05 +0200

    Add services component

---
+++ src/components/Services/Services.tsx
@@ -0,0 +1,35 @@
+import ServicesStyled from "./ServicesStyled";
+import { useTranslation } from "react-i18next";
+
+const Services = (): React.ReactElement => {
+  const [t] = useTranslation("home");
+
+  return (
+    <ServicesStyled>
+      <article className="card">
+        <img
+          className="card__icon"
+          src="/images/icon-house2.webp"
+          alt="A house icon"
+          width="329"
+          height="290"
+        />
+        <h2 className="card__title">{t("services.home")}</h2>
+        <p className="card__text">{t("services.textHome")}</p>
+      </article>
+      <article className="card">
+        <img
+          className="card__icon"
+          src="/images/icon-shop.webp"
+          alt="A dog collar icon"
+          width="329"
+          height="290"
+        />
+        <h2 className="card__title">{t("services.shop")}</h2>
+        <p className="card__text">{t("services.textShop")}</p>
+      </article>
+    </ServicesStyled>
+  );
+};
+
+export default Services;
