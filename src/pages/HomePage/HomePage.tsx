b72085ee2c0ae3923b157c4f41f735f15b8bf30d
Mela Maluenda <maluenda.mela@gmail.com>
2024-06-20 17:38:19 +0200

    Add home page

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,39 @@
+import { useTranslation } from "react-i18next";
+import Services from "../../components/Services/Services";
+import Us from "../../components/Us/Us";
+import HomeStyled from "./HomeStyled";
+import Visitors from "../../components/Visitors/Visitors";
+
+const HomePage = (): React.ReactElement => {
+  const [t] = useTranslation("home");
+
+  return (
+    <HomeStyled>
+      <div className="title">
+        <img
+          className="title__footprint"
+          src="/images/dog-footprint.webp"
+          alt="A dog footprint"
+          width="54"
+          height="47"
+        />
+        <h2 className="title__label"> {t("services.title")} </h2>
+      </div>
+      <Services />
+      <Us />
+      <div className="title">
+        <img
+          className="title__footprint"
+          src="/images/dog-footprint.webp"
+          alt="A dog footprint"
+          width="54"
+          height="47"
+        />
+        <h2 className="title__label"> {t("visitors.title")} </h2>
+      </div>
+      <Visitors />
+    </HomeStyled>
+  );
+};
+
+export default HomePage;
