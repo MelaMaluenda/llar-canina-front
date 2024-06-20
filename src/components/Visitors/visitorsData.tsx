41fbe23914ae6e4f45ee72286b7100d789218d59
Mela Maluenda <maluenda.mela@gmail.com>
2024-06-20 17:39:17 +0200

    Add visitors component carroussel

---
+++ src/components/Visitors/visitorsData.tsx
@@ -0,0 +1,34 @@
+export const visitorsData = [
+  {
+    id: 1,
+    imgUrl: "images/visitor1.jpeg",
+  },
+  {
+    id: 2,
+    imgUrl: "images/visitor2.jpeg",
+  },
+  {
+    id: 3,
+    imgUrl: "images/visitor3.jpeg",
+  },
+  {
+    id: 4,
+    imgUrl: "images/visitor4.jpeg",
+  },
+  {
+    id: 5,
+    imgUrl: "images/visitor5.jpeg",
+  },
+  {
+    id: 6,
+    imgUrl: "images/visitor6.jpeg",
+  },
+  {
+    id: 7,
+    imgUrl: "images/visitor7.jpeg",
+  },
+  {
+    id: 8,
+    imgUrl: "images/visitor8.jpeg",
+  },
+];
