a32ecef6c59e21c09adc9402128493756163a23e
Mela Maluenda <maluenda.mela@gmail.com>
2024-07-16 21:07:01 +0200

    Modify translate buttons

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,22 +2,31 @@ import NavigationStyled from "./NavigationStyled";
 import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
+const languages = [
+  { code: "cat", lang: "CAT" },
+  { code: "es", lang: "ES" },
+];
+
 const Navigation = (): React.ReactElement => {
-  const [t, i18n] = useTranslation("home");
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng: string | undefined) => {
+    i18n.changeLanguage(lng);
+  };
 
   return (
     <NavigationStyled className="menu">
       <div className="menu__head logo">
         <img
           className="logo__image"
-          src="/images/logo-color.png"
+          src="/images/logo.webp"
           alt="Logo with a dog"
           width="150"
           height="160"
         />
         <img
           className="logo__text"
-          src="/images/letra-logo3.png"
+          src="/images/letra-logo1.webp"
           alt="Llar canina. Més que un animal de companyia"
           width="300"
           height="75"
@@ -41,18 +50,15 @@ const Navigation = (): React.ReactElement => {
         </li>
       </ul>
       <div className="menu__languages languages">
-        <button
-          className="menu__languages--esp"
-          onClick={() => i18n.changeLanguage("esp")}
-        >
-          ES
-        </button>
-        <button
-          className="menu__languages--cat"
-          onClick={() => i18n.changeLanguage("cat")}
-        >
-          CAT
-        </button>
+        {languages.map((lng) => (
+          <button
+            key={lng.code}
+            className="menu__languages--i"
+            onClick={() => changeLanguage(lng.code)}
+          >
+            {lng.lang}
+          </button>
+        ))}
       </div>
     </NavigationStyled>
   );
