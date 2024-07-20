c626b9b8bc46517fc2c05fb43883db19401f9502
Mela Maluenda <maluenda.mela@gmail.com>
2024-07-20 03:48:21 +0200

    Add styles to carousel and visitors cmponents

--- src/components/Visitors/Visitors.tsx
+++ src/components/Visitors/Visitors.tsx
@@ -1,73 +1,11 @@
-import React, { useRef, useEffect, useState } from "react";
-import VisitorsStyled from "./VisitorsStyled";
+import CarouselComponent from "./CarouselComponent";
 import { visitorsData } from "./visitorsData";
+import VisitorsStyled from "./VisitorsStyled";
 
-const Visitors = (): React.ReactElement => {
-  const carrouselRef = useRef<HTMLUListElement>(null);
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const listNode = carrouselRef.current;
-    const imgNode = listNode?.querySelectorAll("li > img")[
-      currentIndex
-    ] as HTMLElement;
-
-    if (imgNode && listNode?.parentElement) {
-      const offset = imgNode.offsetLeft - listNode.offsetLeft;
-      listNode.parentElement.scrollTo({
-        left: offset,
-        behavior: "smooth",
-      });
-    }
-  }, [currentIndex]);
-
-  const scrollToImage = (direction: string) => {
-    if (direction === "prev") {
-      setCurrentIndex((current) =>
-        current === 0 ? visitorsData.length - 4 : current - 1,
-      );
-    } else if (direction === "next") {
-      setCurrentIndex((current) =>
-        current === visitorsData.length - 1 ? 0 : current + 1,
-      );
-    }
-  };
-
+const Visitors: React.FC = () => {
   return (
-    <VisitorsStyled className="container">
-      <div className="container__carrousel">
-        <button
-          onClick={() => scrollToImage("prev")}
-          className="button button__left"
-          aria-label="Move carrousel left"
-        >
-          &#10094;
-        </button>
-        <button
-          onClick={() => scrollToImage("next")}
-          className="button button__right"
-          aria-label="Move carrousel right"
-        >
-          &#10095;
-        </button>
-        <div className="slider-wrapper">
-          <ul ref={carrouselRef} className="slider">
-            {visitorsData.map((item) => {
-              return (
-                <li key={item.id}>
-                  <img
-                    className="slider__section"
-                    src={item.imgUrl}
-                    alt="Visitor"
-                    width="378"
-                    height="504"
-                  />
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
+    <VisitorsStyled>
+      <CarouselComponent items={visitorsData} />
     </VisitorsStyled>
   );
 };
--- src/components/Visitors/VisitorsStyled.ts
+++ src/components/Visitors/VisitorsStyled.ts
@@ -1,79 +1,9 @@
+import { Container } from "@mui/material";
 import styled from "styled-components";
 
-const VisitorsStyled = styled.div`
-  padding: 10px 40px;
-  display: flex;
-  align-items: center;
-  position: relative;
-
-  .container {
-    width: 100%;
-    max-width: 1500px; // Ajusta según el diseño deseado
-    height: 500px;
-    box-shadow: 0 8px 32px 0 ${({ theme }) => theme.colors.lighterBackground};
-    border-radius: 60px;
-    overflow: hidden;
-    position: relative;
-
-    &__carrousel {
-      width: 100%;
-      position: relative;
-    }
-  }
-
-  .slider-wrapper {
-    width: 100%;
-    overflow: hidden;
-  }
-
-  .slider {
-    display: flex;
-    transition: transform 0.5s ease-in-out;
-    padding: 0;
-    margin: 0;
-    list-style: none;
-
-    &__section {
-      margin: 10px 15px;
-      width: 308px;
-      height: 434px;
-      object-fit: cover;
-      border-radius: 60px;
-    }
-  }
-
-  .button {
-    display: flex;
-    position: absolute;
-    top: 50%;
-    font-size: ${({ theme }) => theme.typography.superSize};
-    background-color: transparent;
-    border-radius: 50%;
-    padding: 10px;
-    font-weight: 600;
-    cursor: pointer;
-    color: ${({ theme }) => theme.colors.light};
-    background-color: ${({ theme }) => theme.colors.darkBlueBackground};
-    transform: translate(0, -50%);
-    transition: 0.5s ease;
-    user-select: none;
-    border: none;
-
-    &__left {
-      left: 10px;
-      padding-right: 15px;
-    }
-
-    &__right {
-      right: 10px;
-      padding-left: 15px;
-    }
-
-    &__left:hover,
-    &__right:hover {
-      background-color: ${({ theme }) => theme.colors.lighterBackground};
-    }
-  }
+const VisitorsStyled = styled(Container)`
+  max-width: 1200px;
+  margin: auto;
 `;
 
 export default VisitorsStyled;
