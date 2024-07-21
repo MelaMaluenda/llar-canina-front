cfa8cb9e45743c5c1185d49c6bbbf101bacd7997
Mela Maluenda <maluenda.mela@gmail.com>
2024-07-21 21:47:14 +0200

    Modify documents namming to carousel visitors

--- src/components/Visitors/VisitorsStyled.ts
+++
@@ -1,9 +0,0 @@
-import { Container } from "@mui/material";
-import styled from "styled-components";
-
-const VisitorsStyled = styled(Container)`
-  max-width: 1200px;
-  margin: auto;
-`;
-
-export default VisitorsStyled;
