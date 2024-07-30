968c905f49b82588c766f359caa92691504d992c
Mela Maluenda <maluenda.mela@gmail.com>
2024-07-31 00:16:01 +0200

    Add services page to app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,8 @@
+import ContactPage from "../../pages/ContactPage/ContactPage";
 import HomePage from "../../pages/HomePage/HomePage";
+import RatesPage from "../../pages/RatesPage/RatesPage";
+import ServicesPage from "../../pages/ServicesPage/ServicesPage";
+import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import { Route, Routes, Navigate } from "react-router-dom";
 
@@ -10,8 +14,12 @@ const App = (): React.ReactElement => {
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<HomePage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/rates" element={<RatesPage />} />
+          <Route path="/contact" element={<ContactPage />} />
         </Routes>
       </main>
+      <Footer />
     </>
   );
 };
