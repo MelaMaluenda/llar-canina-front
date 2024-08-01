aabb7b9ce059e5488a88b3c00de367f45c2a1b30
Mela Maluenda <maluenda.mela@gmail.com>
2024-08-02 01:45:57 +0200

    Add reservation form to contact page

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,7 +1,14 @@
+import ReservationForm from "../../components/ReservationForm/ReservationForm";
 import ContactStyled from "./ContactStyled";
+import Location from "../../components/Location /Location";
 
 const ContactPage = (): React.ReactElement => {
-  return <ContactStyled></ContactStyled>;
+  return (
+    <ContactStyled>
+      <ReservationForm />
+      <Location />
+    </ContactStyled>
+  );
 };
 
 export default ContactPage;
