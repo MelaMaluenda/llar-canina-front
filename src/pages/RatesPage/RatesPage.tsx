1f3f97b85322d9f34c4b931db2f30439a215afa0
Mela Maluenda <maluenda.mela@gmail.com>
2024-08-03 02:50:02 +0200

    Add rates styles and ratespage

--- src/pages/RatesPage/RatesPage.tsx
+++ src/pages/RatesPage/RatesPage.tsx
@@ -1,7 +1,18 @@
 import RatesStyled from "./RatesStyled";
+import Location from "../../components/Location /Location";
+import Rates from "../../components/Rates/Rates";
+import Requirements from "../../components/Requirements/Requirements";
+import CallToAction from "../../components/CallToAction/CallToAction";
 
 const RatesPage = (): React.ReactElement => {
-  return <RatesStyled></RatesStyled>;
+  return (
+    <RatesStyled>
+      <Rates />
+      <CallToAction />
+      <Requirements />
+      <Location />
+    </RatesStyled>
+  );
 };
 
 export default RatesPage;
