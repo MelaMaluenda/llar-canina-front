9c18f88c167435bcd28e031f14138ab653eec527
Mela Maluenda <maluenda.mela@gmail.com>
2024-08-07 06:41:58 +0200

    Add responsive images to carousel reviews

--- src/components/CarouselReviews/CarouselReviewsData.tsx
+++ src/components/CarouselReviews/CarouselReviewsData.tsx
@@ -1,16 +1,16 @@
 const CarouselReviewsData = [
   {
-    imgUrl: "images/reviewer1.jpg",
+    imgUrl: "https://i.ibb.co/GF2Shhg/reviewer1-180.webp",
     nameKey: "reviews.person1",
     textKey: "reviews.review1",
   },
   {
-    imgUrl: "images/reviewer2.jpg",
+    imgUrl: "https://i.ibb.co/M9n5dDN/reviewer2-180.webp",
     nameKey: "reviews.person2",
     textKey: "reviews.review2",
   },
   {
-    imgUrl: "images/reviewer3.jpg",
+    imgUrl: "https://i.ibb.co/TcXHGV6/reviewer3-180.webp",
     nameKey: "reviews.person3",
     textKey: "reviews.review3",
   },
