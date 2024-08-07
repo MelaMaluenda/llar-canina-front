8bf6e6962e6f57431c1a11154312b6f00209a0d8
Mela Maluenda <maluenda.mela@gmail.com>
2024-08-07 06:39:57 +0200

    Add responsive images to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
 import FooterStyled from "./FooterStyled";
+import ResponsiveImage from "../ResponsiveImage/ResponsiveImage";
 
 const Footer = () => {
   const { t } = useTranslation("translation");
@@ -28,23 +29,27 @@ const Footer = () => {
         <ul className="footer__author">
           <li>
             <a href="https://www.instagram.com/llarcanina" target="_blank">
-              <img
-                src="images/camara.png"
-                alt="Instagram Llar Canina"
+              <ResponsiveImage
                 className="footer__author--icons"
-                width={35}
-                height={35}
+                imgUrl={{
+                  small: "https://i.ibb.co/WtdG9cs/camara-320.webp",
+                  medium: "https://i.ibb.co/WtdG9cs/camara-320.webp",
+                  large: "https://i.ibb.co/rw0S9qN/camara-768.webp",
+                }}
+                alt="Instagram Llar Canina"
               />
             </a>
           </li>
           <li>
             <a href="mailto:info@llarcanina.cat">
-              <img
-                src="images/email2.png"
-                alt="Enviar correo electrónico"
+              <ResponsiveImage
                 className="footer__author--icons"
-                width={40}
-                height={35}
+                imgUrl={{
+                  small: "https://i.ibb.co/2tkhhq3/email2-320.webp",
+                  medium: "https://i.ibb.co/2tkhhq3/email2-320.webp",
+                  large: "https://i.ibb.co/2WbRZXq/email2-768.webp",
+                }}
+                alt="Enviar correo electrónico"
               />
             </a>
           </li>
